244c5b6a2fa9938b4c896ba991d8d70708518861
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-01-30 21:26:48 +0100

    Add code

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("AzureAppConfigConnectionString_StefHeyenrath"));
+
+            var config = builder.Build();
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        }
+    }
+}
